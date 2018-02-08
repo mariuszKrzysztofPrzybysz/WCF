1df156957d10426f2b2a5410675e79c7ea556f30
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2018-02-08 20:39:29 +0100

    Hostowanie usługi - Serwis Windows

---
+++ CategoryInformationHosts.WindowsService/CategoryInformationWSService.cs
@@ -0,0 +1,45 @@
+﻿namespace CategoryInformationHosts.WindowsService
+{
+    using System.ServiceProcess;
+    using System.ServiceModel;
+    using CategoryInformationService.ServiceImplementations;
+    using CategoryInformationService.ServiceContracts;
+
+    public partial class CategoryInformationWSService : ServiceBase
+    {
+        public CategoryInformationWSService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) => RunHost();
+
+        protected override void OnStop()
+        {
+            if (_host != null)
+            {
+                _host.Close();
+                _host = null;
+            }
+        }
+
+        private void RunHost()
+        {
+            _host = new ServiceHost(typeof(InMemoryCategoryInformationService));
+            _host.Open();
+        }
+
+        private void RunInlineConfiguredHost()
+        {
+            var baseAddr = @"http://localhost:8733/Design_Time_Addresses/CategoryInformationService/InMemoryCategoryInformationService/";
+            _host = new ServiceHost(typeof(InMemoryCategoryInformationService));
+
+            //Add endpoint
+            _host.AddServiceEndpoint(typeof(ICategoryInformationService),
+                new BasicHttpBinding(),
+                baseAddr);
+
+            _host.Open();
+        }
+    }
+}
---
+++ CategoryInformationHosts.WindowsService/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CategoryInformationHosts.WindowsService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryInformationServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.categoryInformationServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // categoryInformationServiceProcessInstaller
+            // 
+            this.categoryInformationServiceProcessInstaller.Password = null;
+            this.categoryInformationServiceProcessInstaller.Username = null;
+            // 
+            // categoryInformationServiceInstaller
+            // 
+            this.categoryInformationServiceInstaller.Description = "CategoryInformationService available via Windows Service. Allows to get some info" +
+    "rmation about persons";
+            this.categoryInformationServiceInstaller.DisplayName = "CategoryInformationService via Windows Service";
+            this.categoryInformationServiceInstaller.ServiceName = "CategoryInformationWSService";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.categoryInformationServiceProcessInstaller,
+            this.categoryInformationServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller categoryInformationServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller categoryInformationServiceInstaller;
+    }
+}
