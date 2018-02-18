4bfedf313e06da4cc10d88b3a3060fb4714839b8
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2018-02-18 21:30:02 +0100

    Tworzenie klienta - Service Reference

---
+++ CategoryInformationClients.ServiceReferenceClient/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CategoryInformationClients.ServiceReferenceClient
+{
+    using System;
+    using CategoryInformationServiceReference;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (CategoryInformationServiceClient client = new CategoryInformationServiceClient("BasicHttpBinding_ICategoryInformationService"))
+            {
+                var responce = client.GetCategoryInformation(new CategoryInformationRequest
+                {
+                    CategoryId = 1
+                });
+
+                Console.WriteLine("Received responce");
+                foreach (var category in responce.Categories)
+                {
+                    Console.WriteLine($"Id: {category.CategoryId}");
+                    Console.WriteLine($"Name: {category.Name}");
+                    Console.WriteLine($"Description: {category.Description}");
+                    Console.WriteLine($"Photo: {category.Photo}");
+
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
